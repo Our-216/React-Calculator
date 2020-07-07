4bb0e63979c8c1c6f52863ed94fb901aeadc7f2f
dreamerryao <244791255@qq.com>
2020-07-07 10:01:47 +0800

    change pages

---
+++ precache-manifest.19433563c8da5bfcfbbcebaf2a03d895.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56881f601fce6f7253e7cfc92a40038b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed2857809a5c8096eef5",
+    "url": "./static/js/2.18f8378b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.18f8378b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e1d40df10a359cd1ac",
+    "url": "./static/js/main.c0247ac9.chunk.js"
+  },
+  {
+    "revision": "99d5c34ba17a5aedc05b",
+    "url": "./static/js/runtime-main.3c3fe3c3.js"
+  }
+]);
